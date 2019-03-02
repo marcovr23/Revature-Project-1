4a0a2a51423ed7b2164bf9da6e7169d3fab4bb7f
Edward <edward.bechtold@gmail.com>
2019-03-02 17:02:06 -0500

    Models started

---
+++ ERS_Project_1/src/main/java/com/revature/models/Reimbursement.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+public class Reimbursement {
+
+	private int reimb_id;
+	private int account;
+	private int submitted;
+	private int resolved; 
+	
+}
---
+++ ERS_Project_1/src/main/java/com/revature/models/User.java
@@ -0,0 +1,12 @@
+package com.revature.models;
+
+public class User {
+
+	private int id;
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String roleId;
+}
