305516598cdf4eb8e12b88344e93d16e93c58883
Edward <edward.bechtold@gmail.com>
2019-03-07 22:49:36 -0500

    UserService progress

--- ERS_Project_1/src/main/java/com/revature/services/UserService.java
+++ ERS_Project_1/src/main/java/com/revature/services/UserService.java
@@ -1,5 +1,56 @@
 package com.revature.services;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.revature.dao.UserDAO;
+import com.revature.models.User;
+
 public class UserService {
 
+	private static Logger log = Logger.getLogger(UserService.class);
+
+	private UserDAO userDao = new UserDAO();
+//	private RoleDAO roleDao = new RoleDAO();
+	
+	public List<User> getAllUsers() {
+		return userDao.getAll();
+	}	
+	
+	public User getByCredentials(String username, String password) {
+		
+		User user = null;
+		
+		if (!username.equals("") && !password.equals("")) {
+			user = userDao.getByCredentials(username, password);
+			// finish this -- there's no verification
+			return user; 
+		}
+		
+		log.info("Empty username and/or password");
+		return null;
+	}
+	
+	public User getUserById(int userId) {
+		return userDao.getById(userId);
+	}
+	
+	public User addUser(User newUser) {
+		if (newUser.getUsername().equals("") || 
+				newUser.getPassword().equals("") || 
+				newUser.getFirstname().equals("") || 
+				newUser.getLastname().equals("")) {
+			log.info("User missing fields");
+			return null;
+		}
+		return userDao.add(newUser);
+	}
 }
+
+/*
+	- Get all;;
+	- getByCredentials;;
+	- add;;
+	- getById;;
+ */
