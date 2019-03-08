b7e28f3bac23525ab4b8af8010d7de00f9a4fd8f
Edward <edward.bechtold@gmail.com>
2019-03-08 14:38:22 -0500

    UserService changes

--- ERS_Project_1/src/main/java/com/revature/service/UserService.java
+++ ERS_Project_1/src/main/java/com/revature/service/UserService.java
@@ -1,6 +1,36 @@
 package com.revature.service;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.revature.dao.UserDAO;
+import com.revature.models.User;
+
 public class UserService {
+	
+	private static Logger log = Logger.getLogger(UserService.class);
+		private UserDAO userDao = new UserDAO();
+		
+		public List<User> getAllUsers() {
+			return userDao.getAll();
+		}
+		
+		public User getUserByCredentials(String username, String password) {
+			
+			User user = null;
+			
+			if(!username.equals("") && !password.equals("")) {
+				user = userDao.getByCredentials(username, password);
+//				if(user != null) 
+//				user.setWishlist(wishlistDao.getUserWishList(user.getId())
+//						.getUserWishList());
+				return user;
+		}
+		log.info("Empty username and/or password");
+		return null;
+		
+}
 	/*
 	 * 
 	 			Example of how to encrypt and decrypt strings
@@ -12,5 +42,14 @@ public class UserService {
 	
 	System.out.println("original string " + pass + "\n encrpted " + encrypt + "\n decrypted " + decrypt);
 	 */
+		
 	
-}
+	
+
+
+/*
+	- add
+	- getall
+	- getbycreds
+	- getbyid
+*/
