aad65bcb1c98b81246b776cd851953f51cb3032c
Marco Van Rhyn <marcovanrhynwow@gmail.com>
2019-03-10 15:21:28 -0400

    fixed inline js on employee.js

--- ERS_Project_1/src/main/webapp/js/employee.js
+++ ERS_Project_1/src/main/webapp/js/employee.js
@@ -1,3 +1,5 @@
+document.getElementById('open-btn').addEventListener('click', openNav);
+document.getElementById('close-btn').addEventListener('click',closeNav);
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
