7f8e85d27788203a3064e1b56911e48c799961e4
Edward <edward.bechtold@gmail.com>
2019-03-11 20:31:12 -0400

    Changed principal

--- ERS_Project_1/src/main/java/com/revature/models/Principal.java
+++ ERS_Project_1/src/main/java/com/revature/models/Principal.java
@@ -3,11 +3,24 @@ package com.revature.models;
 public class Principal {
 	
 	private int id;
-	private String role;
 	private String username;
-	//tracy just had String password instead of username
+	private String role;
+	
 	public Principal() {
 		super();
+	}
+
+	public Principal(int id, String role) {
+		super();
+		this.id = id;
+		this.role = role;
+	}
+	
+	public Principal(int id, String username, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.role = role;
 	}
 
 	public int getId() {
@@ -18,6 +31,14 @@ public class Principal {
 		this.id = id;
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 	public String getRole() {
 		return role;
 	}
@@ -26,23 +47,43 @@ public class Principal {
 		this.role = role;
 	}
 
-	public String getUsername() {
-		return username;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public Principal(int id, String role, String username) {
-		super();
-		this.id = id;
-		this.role = role;
-		this.username = username;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Principal other = (Principal) obj;
+		if (id != other.id)
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Principal [id=" + id + ", role=" + role + ", username=" + username + "]";
+		return "Principal [id=" + id + ", username=" + username + ", role=" + role + "]";
 	}
+
 }
